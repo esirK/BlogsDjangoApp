0a0662e39f6679fdffb359ee80b692386639e4bb
esir <esirkings@gmail.com>
2018-11-10 08:00:37 +0300

    Adds views for posts

--- blog/urls.py
+++ blog/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from .views import post_list, post_detail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('posts/', post_list, name="posts"),
+    path('posts/<slug>/', post_detail, name="post"),
 ]
